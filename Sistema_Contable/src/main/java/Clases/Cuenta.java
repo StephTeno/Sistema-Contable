6a674cb66ad8f1036f70d9e75343d633dd2f8003
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-11 20:27:48 -0600

    Con

--- Sistema_Contable/src/main/java/Clases/Cuenta.java
+++ Sistema_Contable/src/main/java/Clases/Cuenta.java
@@ -5,33 +5,33 @@ public class Cuenta {
     private double Cantidad;
     
     public Cuenta(String no_Cuenta, String nombre_Cuenta, double cantidad) {
-        No_Cuenta = no_Cuenta;
-        Nombre_Cuenta = nombre_Cuenta;
-        Cantidad = cantidad;
+        this.No_Cuenta = no_Cuenta;
+        this.Nombre_Cuenta = nombre_Cuenta;
+        this.Cantidad = cantidad;
     }
 
     public String getNo_Cuenta() {
-        return No_Cuenta;
+        return this.No_Cuenta;
     }
 
     public void setNo_Cuenta(String no_Cuenta) {
-        No_Cuenta = no_Cuenta;
+        this.No_Cuenta = no_Cuenta;
     }
 
     public String getNombre_Cuenta() {
-        return Nombre_Cuenta;
+        return this.Nombre_Cuenta;
     }
 
     public void setNombre_Cuenta(String nombre_Cuenta) {
-        Nombre_Cuenta = nombre_Cuenta;
+        this.Nombre_Cuenta = nombre_Cuenta;
     }
 
     public double getCantidad() {
-        return Cantidad;
+        return this.Cantidad;
     }
 
     public void setCantidad(double cantidad) {
-        Cantidad = cantidad;
+        this.Cantidad = cantidad;
     }
 
     
