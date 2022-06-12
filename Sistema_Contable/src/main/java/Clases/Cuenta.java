1c4159e095e3620be7220b6c0ae302ece17c1cc4
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-11 20:07:16 -0600

    Conf

--- Sistema_Contable/src/main/java/Clases/Cuenta.java
+++ Sistema_Contable/src/main/java/Clases/Cuenta.java
@@ -2,9 +2,37 @@ package Clases;
 public class Cuenta {
     private String No_Cuenta;
     private String Nombre_Cuenta;
-
-    public Cuenta(String no_Cuenta, String nombre_Cuenta) {
+    private double Cantidad;
+    
+    public Cuenta(String no_Cuenta, String nombre_Cuenta, double cantidad) {
         No_Cuenta = no_Cuenta;
         Nombre_Cuenta = nombre_Cuenta;
+        Cantidad = cantidad;
     }
+
+    public String getNo_Cuenta() {
+        return No_Cuenta;
+    }
+
+    public void setNo_Cuenta(String no_Cuenta) {
+        No_Cuenta = no_Cuenta;
+    }
+
+    public String getNombre_Cuenta() {
+        return Nombre_Cuenta;
+    }
+
+    public void setNombre_Cuenta(String nombre_Cuenta) {
+        Nombre_Cuenta = nombre_Cuenta;
+    }
+
+    public double getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        Cantidad = cantidad;
+    }
+
+    
 }
