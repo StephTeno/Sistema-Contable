4397adffc87413847a123cc15a20285b4e0fd63a
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-11 19:57:05 -0600

    Conf

--- Sistema_Contable/src/main/java/Clases/Cuenta.java
+++ Sistema_Contable/src/main/java/Clases/Cuenta.java
@@ -2,5 +2,9 @@ package Clases;
 public class Cuenta {
     private String No_Cuenta;
     private String Nombre_Cuenta;
-    
+
+    public Cuenta(String no_Cuenta, String nombre_Cuenta) {
+        No_Cuenta = no_Cuenta;
+        Nombre_Cuenta = nombre_Cuenta;
+    }
 }
