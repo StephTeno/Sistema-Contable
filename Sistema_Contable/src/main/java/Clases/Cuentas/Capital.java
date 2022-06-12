c7ad446565b1a7ed2d94a6f987933c714a03ba5a
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-11 21:08:32 -0600

    Confi

---
+++ Sistema_Contable/src/main/java/Clases/Cuentas/Capital.java
@@ -0,0 +1,9 @@
+package Clases.Cuentas;
+public class Capital extends Cuenta{
+
+    public Capital(String no_Cuenta, String nombre_Cuenta, String clasificacion, String sub_clasificacion,
+            double cantidad) {
+        super(no_Cuenta, nombre_Cuenta, clasificacion, sub_clasificacion, cantidad);
+    }
+    
+}
