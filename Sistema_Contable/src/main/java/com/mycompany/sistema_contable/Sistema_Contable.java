cbbe16dad98c50b6ada211290f644b2c98076b26
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-16 18:11:50 -0600

    Entrar

--- Sistema_Contable/src/main/java/com/mycompany/sistema_contable/Sistema_Contable.java
+++ Sistema_Contable/src/main/java/com/mycompany/sistema_contable/Sistema_Contable.java
@@ -1,7 +1,26 @@
 package com.mycompany.sistema_contable;
+import java.util.*;
 public class Sistema_Contable {
 
     public static void main(String[] args) {
+        Scanner l=new Scanner(System.in);
+        boolean salir=false;
+        int op;
+
+
+        do{
+            while(!l.hasNextInt())                             
+            {
+                System.out.println("Ingrese su opción: ");
+                l.next();
+            }
         
+            op = l.nextInt();
+            switch(op){
+
+            }
+
+        }while(!salir);
+        l.close();
     }
 }
