0488661f7ad23651f1be22a420ee8322470825ec
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-20 19:44:07 -0600

    Act.3

--- Sistema_Contable/src/main/java/Clases/Extra/Ventana.java
+++ Sistema_Contable/src/main/java/Clases/Extra/Ventana.java
@@ -1,26 +1,29 @@
 package Clases.Extra;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*; 
-public class Ventana extends JFrame
-{
-/*      private JButton jbtn_boton;
-        private JTextField jtxt_texto;
-        private JLabel jlbl_label; */
+/* import java.awt.event.*;  */
+public class Ventana extends JFrame{
+/*         private JButton jbtn_boton;
+        private JTextField jtxt_texto; */
         private void IniciarComponentes(){
                 JPanel panel=new JPanel();
+                panel.setLayout(null);
                 panel.setBackground(Color.decode("#99CCFF"));
-                this.getContentPane().add(panel);
+                JLabel Etiqueta=new JLabel();
+                Etiqueta.setText("Hola");
+                Etiqueta.setBounds(10, 10, 50, 60);
+                Etiqueta.setForeground(Color.WHITE); //Color del font
+                panel.add(Etiqueta);
+                this.getContentPane().add(Etiqueta);
         }
         public Ventana(String Titulo) {
                 this.setTitle(Titulo);
                 this.getContentPane().setBackground(Color.decode("#99CCFF"));
-                this.setSize(500, 800);
-                this.setLayout( new FlowLayout() );
+                this.setSize(800, 600);
                 IniciarComponentes();
                 this.setVisible( true );
                 this.setLocationRelativeTo(null);
-                this.setResizable(false);
+                //this.setResizable(false);
                 this.setMinimumSize(new Dimension(200,200));
                 this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
         }
---
+++ Sistema_Contable/src/main/java/com/mycompany/sistema_contable/algo.java
@@ -0,0 +1,9 @@
+package com.mycompany.sistema_contable;
+
+import Clases.Extra.Ventana;
+
+public class algo {
+    public static void main(String[] args) {
+        Ventana v1= new Ventana("Menú");
+    }
+}
