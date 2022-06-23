d8af225cc67276be9065e73669210cb71247bfef
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-22 18:44:25 -0600

    Act.5

--- Sistema_Contable/src/main/java/com/mycompany/sistema_contable/Sistema_Contable.java
+++ Sistema_Contable/src/main/java/com/mycompany/sistema_contable/Sistema_Contable.java
@@ -37,28 +37,52 @@ public class Sistema_Contable {
 
     public static void main(String[] args) {
         Scanner l=new Scanner(System.in);
-        ArrayList <Cuenta> cuenta=new ArrayList<>();
+        //ArrayList <Cuenta> cuenta=new ArrayList<>();
         boolean salir=false;
-        String Botones[]={"Compra", "Pago de Gasto", };
-        int op;
-        do{
-            op = Integer.parseInt(JOptionPane.showMessageDialog(null, ("Elije una opcion:\n "
-            + "1)Ingrese los movimientos de transacciones\n "
-            + "2)Balanza de Comprobación\n "
-            + "3)Estado Financieros\n "), "Menú", messageType));
-
-
+        String Botones[]={"Compra", "Pago", "Ventas"}, menu[] ={"Ingrese los movimientos de transacciones","Balanza de Comprobación","Estado Financieros"}, compras[]={"Inventario","Bienes inmuebles"}, pagos[]={"Servicios Básicos"}, venta[]={"Productos"};
+        do{;
+            String op = (String) JOptionPane.showInputDialog(null, "Elija el proceso a mostrar", "MENÚ", JOptionPane.QUESTION_MESSAGE, null, menu, menu[0]);
             switch (op) {
-                case 1:
+                case "Ingrese los movimientos de transacciones":
                     //hd=true;
                     JOptionPane.showMessageDialog(null,"\n\tREGISTRO DE OPERACIONES");
-                    int oP = JOptionPane.showOptionDialog (null, "\nPresione el botón de la transacción realizada", "REGISTRO DE OPERACIONES", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null/*icono*/, Botones, Botones[0]);
-
+                    int oP = JOptionPane.showOptionDialog (null, "\nPresione el botón de la transacción realizada", "REGISTRO DE OPERACIONES", 0 , JOptionPane.WARNING_MESSAGE, null/*icono*/, Botones, Botones[0]);
+                    oP=oP+1;
                     switch (oP) {
                         case 1:
+                            String com = (String) JOptionPane.showInputDialog(null, "¿Qué compró?", "COMPRA", JOptionPane.QUESTION_MESSAGE, null, compras, compras[0]);
+                            switch (com) {
+                                case "Inventario":
+                                    
+                                    break;
                             
+                                default:
+                                    break;
+                            }
                             break;
-                    
+                        case 2:
+                            String pago = (String) JOptionPane.showInputDialog(null, "¿Qué pagó?", "PAGO", JOptionPane.QUESTION_MESSAGE, null, pagos, pagos[0]);
+                            switch (pago) {
+                                case "Inventario":
+                                    
+                                    break;
+                            
+                                default:
+                                    break;
+                            }
+                            break;
+
+                        case 3:
+                            String ven = (String) JOptionPane.showInputDialog(null, "¿Cuanto vendió por producto?", "VENTA", JOptionPane.QUESTION_MESSAGE, null, venta, venta[0]);
+                            switch (ven) {
+                                case "Inventario":
+                                    
+                                    break;
+                            
+                                default:
+                                    break;
+                            }
+                            break;
                         default:
                             break;
                     }
@@ -67,7 +91,7 @@ public class Sistema_Contable {
                     
                     
                     
-                    int No_Cuenta;
+                    /*int No_Cuenta;
                     String Nombre_Cuenta, Clasificacion, Sub_clasificacion;
                     for (int i = 0; i <1; i++) {
                         No_Cuenta=Integer.parseInt(JOptionPane.showInputDialog("\nNumero de Referencia de la Cuenta: "));
@@ -77,17 +101,17 @@ public class Sistema_Contable {
                         cuenta.add(new Cuenta(No_Cuenta, Nombre_Cuenta, Clasificacion, Sub_clasificacion));
                     }
                     JOptionPane.showMessageDialog(null,"\nPRODUCTO REGISTRADOS EXITOSAMENTE \n");
+                    break;*/
+                case "Balanza de Comprobación":
                     break;
-                case 2:
-                    break;
-                case 3:
+                case "Estado Financieros":
                     break;
                 default:
                     System.out.println("**Ha escogido una opción invalida, ¡Intentelo nuevamente!**");
                     break;
             }
 
-        }while(!salir);
+        } while(!salir);
         l.close();
     }
 }
--- Sistema_Contable/src/main/java/com/mycompany/sistema_contable/algo.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.sistema_contable;
-
-import Clases.Extra.Ventana;
-
-public class algo {
-    public static void main(String[] args) {
-        Ventana v1=new Ventana("Hola");
-    }
-}
